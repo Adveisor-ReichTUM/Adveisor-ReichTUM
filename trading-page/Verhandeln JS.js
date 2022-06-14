ccf75f6e295d7c45e6d5e4af74463717c7c37726
Fabian_Gruber <gruberfabian@gmx.net>
2022-06-14 21:26:20 +0200

    loginpage und trading page aktualisiert

--- trading-page/Verhandeln JS.js
+++ trading-page/Verhandeln JS.js
@@ -274,7 +274,6 @@ function update_streets_on_screen(my_cards, opponent_cards, free_cards) {
     wuensche_value = 500;
     show_offer(value_angebote, angebote, value_wuensche, wuensche);
 }
-
 //test
 function submit_trading_offer() {
     my_cards_test = ["street_1", "street_2", "street_3"];
@@ -345,3 +344,58 @@ function show_acception_alert(decision) {
 /*function submit_trading_offer(){
     show_opponent_decision(false);
 }*/
+
+//Alex kriegt Usernames von Login-Page, gibt ihnen playerId und ruft write-. mit den Variablen own-. und opponent-. auf
+function write_username(own_username, opponent_username) {
+    //Spieler 1 für eigenen Nutzernamen
+    document.getElementById("own_username").innerHTML = own_username;
+    //Spieler 2 für gegnerischen Nutzernamen
+    document.getElementById("opponent_username").innerHTML = opponent_username;
+}
+
+/*function show_whos_turn() {
+
+}*/
+
+function open_modal() {
+    // Get the modal
+    var modal = document.getElementById("myModal");
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks the button, open the modal 
+    btn.onclick = function() {
+    modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+    modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+    }
+    hide_modal();
+}
+
+function hide_modal() {
+    var counter = 3;
+    var timer_status = setInterval(function(){
+        counter--;
+        if(!(counter > 0)) {
+            $('#myModal').hide();
+            clearInterval(timer_status);
+        }
+    }, 1000);
+}
+
+
+
