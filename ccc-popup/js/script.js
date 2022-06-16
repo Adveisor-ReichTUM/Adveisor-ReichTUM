a7e93a71a6207d750e4eb209b3a45fbbd7fb9187
lgerst <104018936+lgerst@users.noreply.github.com>
2022-06-16 20:40:57 +0200

    AdvEIsor

--- ccc-popup/js/script.js
+++ ccc-popup/js/script.js
@@ -27,5 +27,58 @@ function evaluateCard(isChanceCard) {
   }
 }
 
+// Get random card
 
+var isChanceCard = 0;
 
+function getRandomCard() {
+  x = Math.floor(Math.random() * 15 + 1);
+  if(isChanceCard == true) {
+    var result = document.getElementById("randomChanceCard");
+    switch (x) {
+      case 1: result.innerHTML="<img alt='Ereigniskarte' src='images/Ereigniskarte_01.jpeg'>"; break;
+      case 2: result.innerHTML="<img alt='Ereigniskarte' src='images/Ereigniskarte_02.jpeg'>"; break;
+      case 3: result.innerHTML="<img alt='Ereigniskarte' src='images/Ereigniskarte_03.jpeg'>"; break;
+      case 4: result.innerHTML="<img alt='Ereigniskarte' src='images/Ereigniskarte_04.jpeg'>"; break;
+      case 5: result.innerHTML="<img alt='Ereigniskarte' src='images/Ereigniskarte_05.jpeg'>"; break;
+      case 6: result.innerHTML="<img alt='Ereigniskarte' src='images/Ereigniskarte_06.jpeg'>"; break;
+      case 7: result.innerHTML="<img alt='Ereigniskarte' src='images/Ereigniskarte_07.jpeg'>"; break;
+      case 8: result.innerHTML="<img alt='Ereigniskarte' src='images/Ereigniskarte_08.jpeg'>"; break;
+      case 9: result.innerHTML="<img alt='Ereigniskarte' src='images/Ereigniskarte_09.jpeg'>"; break;
+      case 10: result.innerHTML="<img alt='Ereigniskarte' src='images/Ereigniskarte_10.jpeg'>"; break;
+      case 11: result.innerHTML="<img alt='Ereigniskarte' src='images/Ereigniskarte_11.jpeg'>"; break;
+      case 12: result.innerHTML="<img alt='Ereigniskarte' src='images/Ereigniskarte_12.jpeg'>"; break;
+      case 13: result.innerHTML="<img alt='Ereigniskarte' src='images/Ereigniskarte_13.jpeg'>"; break;
+      case 14: result.innerHTML="<img alt='Ereigniskarte' src='images/Ereigniskarte_14.jpeg'>"; break;
+      case 15: result.innerHTML="<img alt='Ereigniskarte' src='images/Ereigniskarte_15.jpeg'>"; break;
+    }
+  }
+  else {
+    var result = document.getElementById("randomCommunityChestCard");
+    switch (x) {
+      case 1: result.innerHTML="<img alt='Gemeinschaftskarte' src='images/Gemeinschaftskarte_01.jpeg'>"; break;
+      case 2: result.innerHTML="<img alt='Gemeinschaftskarte' src='images/Gemeinschaftskarte_02.jpeg'>"; break;
+      case 3: result.innerHTML="<img alt='Gemeinschaftskarte' src='images/Gemeinschaftskarte_03.jpeg'>"; break;
+      case 4: result.innerHTML="<img alt='Gemeinschaftskarte' src='images/Gemeinschaftskarte_04.jpeg'>"; break;
+      case 5: result.innerHTML="<img alt='Gemeinschaftskarte' src='images/Gemeinschaftskarte_05.jpeg'>"; break;
+      case 6: result.innerHTML="<img alt='Gemeinschaftskarte' src='images/Gemeinschaftskarte_06.jpeg'>"; break;
+      case 7: result.innerHTML="<img alt='Gemeinschaftskarte' src='images/Gemeinschaftskarte_07.jpeg'>"; break;
+      case 8: result.innerHTML="<img alt='Gemeinschaftskarte' src='images/Gemeinschaftskarte_08.jpeg'>"; break;
+      case 9: result.innerHTML="<img alt='Gemeinschaftskarte' src='images/Gemeinschaftskarte_09.jpeg'>"; break;
+      case 10: result.innerHTML="<img alt='Gemeinschaftskarte' src='images/Gemeinschaftskarte_10.jpeg'>"; break;
+      case 11: result.innerHTML="<img alt='Gemeinschaftskarte' src='images/Gemeinschaftskarte_11.jpeg'>"; break;
+      case 12: result.innerHTML="<img alt='Gemeinschaftskarte' src='images/Gemeinschaftskarte_12.jpeg'>"; break;
+      case 13: result.innerHTML="<img alt='Gemeinschaftskarte' src='images/Gemeinschaftskarte_13.jpeg'>"; break;
+      case 14: result.innerHTML="<img alt='Gemeinschaftskarte' src='images/Gemeinschaftskarte_14.jpeg'>"; break;
+      case 15: result.innerHTML="<img alt='Gemeinschaftskarte' src='images/Gemeinschaftskarte_15.jpeg'>"; break;
+    }
+  }
+  console.log(x);
+}
+
+btn.addEventListener('click', () => {
+  getRandomCard(); 
+});
+
+evaluateCard(isChanceCard);
+getRandomCard();
